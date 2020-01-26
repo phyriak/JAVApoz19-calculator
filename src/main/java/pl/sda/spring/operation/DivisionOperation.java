a26ad990890061d3884072a55bec70c20b9aa1f4
Piotr Hyriak <piotr1hyriak@gmail.com>
2020-01-26 11:59:46 +0100

    Second commit

--- src/main/java/pl/sda/spring/operation/DivisionOperation.java
+++ src/main/java/pl/sda/spring/operation/DivisionOperation.java
@@ -1,14 +1,23 @@
 package pl.sda.spring.operation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pl.sda.spring.exception.DivisionByZeroException;
 
 @Component
 public class DivisionOperation implements Operation {
+
+
+    private final String exceptionMessage;
+
+    public DivisionOperation(String exceptionMessage) {
+        this.exceptionMessage = exceptionMessage;
+    }
+
     @Override
     public double calculate(double arg1, double arg2) throws DivisionByZeroException{
         if(arg2==0){
-            throw new DivisionByZeroException("Divide by zero");
+            throw new DivisionByZeroException(exceptionMessage);
         }
         return arg1/arg2;
     }
--- src/main/java/pl/sda/spring/operation/OperationConfiguration.java
+++ src/main/java/pl/sda/spring/operation/OperationConfiguration.java
@@ -4,10 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-class SubtractionOperationConfiguration {
-
+public class OperationConfiguration {
     @Bean
     SubtractionOperation subtractionOperation() {
         return new SubtractionOperation();
     }
+
+    @Bean
+    DivisionOperation divisionOperation() {
+        return new DivisionOperation("NIe dzielimy przez zero"); }
+
 }
